ec1325726727a2ae5d42a9be67fe8f4f37ff139b
Plamen Petrov <biznetmen@gmail.com>
2024-04-24 17:54:41 +0300

    Understanding the "return" Keyword

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,15 @@
 blockchain = [[0]]
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
 def add_value(value):
-    blockchain.append([blockchain[-1],value])
-    print(blockchain)
+    blockchain.append([get_last_blockchain_value(),value])
+    
 
 add_value(3.14)
 add_value(24.04)
 add_value(2024)
+
+print(blockchain)
