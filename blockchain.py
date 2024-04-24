b723f9e65d778b4856721f44450a711709532c61
Plamen Petrov <biznetmen@gmail.com>
2024-04-24 11:07:22 +0300

    Accessing the Last List Item

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,9 @@
-blockchain = []
+blockchain = [[0]]
 
 def add_value(value):
-    blockchain.append(value)
+    blockchain.append([blockchain[-1],value])
     print(blockchain)
 
 add_value(3.14)
 add_value(24.04)
+add_value(2024)
