6d34db389e172757fb551d4d563302a4b56534f0
Plamen Petrov <biznetmen@gmail.com>
2024-04-25 08:35:33 +0300

    Using Default Arguments

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,15 @@
-blockchain = [[0]]
+blockchain = []
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(value):
-    blockchain.append([get_last_blockchain_value(),value])
+def add_value(value, last_transaction=[1]):
+    blockchain.append([last_transaction,value])
     
 
 add_value(3.14)
-add_value(24.04)
-add_value(2024)
+add_value(24.04,get_last_blockchain_value())
+add_value(2024,get_last_blockchain_value())
 
 print(blockchain)
