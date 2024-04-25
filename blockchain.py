806cc5f076c297aecd31313be1cfd05f5bab3af2
Plamen Petrov <biznetmen@gmail.com>
2024-04-25 08:51:12 +0300

    Avoiding Repetitive Code Execution

--- blockchain.py
+++ blockchain.py
@@ -6,14 +6,17 @@ def get_last_blockchain_value():
 
 def add_value(value, last_transaction=[1]):
     blockchain.append([last_transaction,value])
+
+def get_user_input():
+    return float(input('Please, enter transaction number: '))
     
-tx_amount = float(input('Please, enter transaction number: '))
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input('Please, enter transaction number: '))
+tx_amount = get_user_input()
 add_value(tx_amount,get_last_blockchain_value())
 
-tx_amount = float(input('Please, enter transaction number: '))
+tx_amount = get_user_input()
 add_value(tx_amount,get_last_blockchain_value())
 
 print(blockchain)
