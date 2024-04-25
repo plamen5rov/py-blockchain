f21caf7d8ceb9e89cf92790a587d2d12af5e67be
Plamen Petrov <biznetmen@gmail.com>
2024-04-25 09:09:15 +0300

    Adding Comments and Doc Strings

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,18 @@
+# Initializing our blockchain list
 blockchain = []
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
     return blockchain[-1]
 
 
 def add_value(value, last_transaction=[1]):
+    """ Appends new value of the current blockchain
+    
+    Arguments:
+    :value: The new value
+    :last_transaction: The last blockchain transaction (default [1])
+    """
     blockchain.append([last_transaction,value])
 
 def get_user_input():
