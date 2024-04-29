141017889caaf1e0aa91cd462c888c5f9f6cd1f4
Plamen Petrov <biznetmen@gmail.com>
2024-04-29 10:16:48 +0300

    Improving our Code with Loops & Conditionals

--- blockchain.py
+++ blockchain.py
@@ -3,23 +3,26 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(value, last_transaction=[1]):
+def add_transaction(value, last_transaction):
     """ Appends new value of the current blockchain
     
     Arguments:
     :value: The new value
     :last_transaction: The last blockchain transaction (default [1])
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction,value])
 
 def get_transaction_value():
     return float(input('Please, enter transaction number: '))
     
-tx_amount = get_transaction_value()
-add_value(tx_amount)
+
 
 
 def get_user_choice():
@@ -39,7 +42,7 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount,get_last_blockchain_value())
+        add_transaction(tx_amount,get_last_blockchain_value())
     elif user_choice == '2':
         print_blocks()
     elif user_choice == 'q':
