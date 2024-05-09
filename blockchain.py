97be32101cff41a78f43c33c029c4eaec1a93c40
Plamen Petrov <biznetmen@gmail.com>
2024-05-09 09:57:43 +0300

    Verifying Transactions

--- blockchain.py
+++ blockchain.py
@@ -18,6 +18,8 @@ def hash_block(block):
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant]for block in blockchain]
+    open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
+    tx_sender.append(open_tx_sender)
     amount_sent = 0
     for tx in tx_sender:
         if len(tx) > 0:
@@ -36,7 +38,12 @@ def get_last_blockchain_value():
         return None
     return blockchain[-1]
 
-
+def verify_transaction(transaction):
+    sender_balance = get_balance(transaction['sender'])
+    return sender_balance >= transaction['amount']
+        
+    
+    
 def add_transaction(recipient,sender=owner, amount=1.0):
     """ Appends new value of the current blockchain
     
@@ -50,10 +57,12 @@ def add_transaction(recipient,sender=owner, amount=1.0):
         'recipient': recipient, 
         'amount': amount
         }
-    
-    open_transactions.append(transaction)
-    participants.add(sender)
-    participants.add(recipient)
+    if verify_transaction(transaction):
+        open_transactions.append(transaction)
+        participants.add(sender)
+        participants.add(recipient)
+        return True
+    return False
     
     
 def mine_block():
@@ -119,7 +128,10 @@ while waiting_for_input:
     if user_choice == '1':
         tx_data = get_transaction_value()
         recipient, amount = tx_data
-        add_transaction(recipient, amount=amount)
+        if add_transaction(recipient, amount=amount):
+            print('Transaction Added!')
+        else:
+            print('Transaction FAILED!')
         print(open_transactions)
     elif user_choice == '2':
         if mine_block():
