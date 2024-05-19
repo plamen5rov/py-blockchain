b2f96b88edad6f187c030d187ed41baaeb73f2b4
Plamen Petrov <biznetmen@gmail.com>
2024-05-19 11:16:57 +0300

    Assignment Solution

---
+++ task1.py
@@ -0,0 +1,20 @@
+import random
+import datetime
+
+n = 1
+
+while n <= 10:
+    random_year = random.randrange(2000,2024)
+    random_month = random.randrange(1,12)
+    random_day = random.randrange(1,29)
+    first_random = random.random()
+    second_random = random.randrange(1, 10)
+    random_date = datetime.date(random_year,random_month,random_day)
+    print('TODAY IS ', random_date)
+    print('Random number between 0 and 1 is ', first_random)
+    print('Random number between 1 and 10 is %d' % second_random)
+    print('*' * 44)
+    n += 1
+    
+print('Random number program ENDED!')
+
