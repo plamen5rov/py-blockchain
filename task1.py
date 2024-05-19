d3c12fee1f1faa84836408c217d3cd3a35f7e007
Plamen Petrov <biznetmen@gmail.com>
2024-05-19 11:20:10 +0300

    Refactoring

--- task1.py
+++ task1.py
@@ -11,7 +11,7 @@ while n <= 10:
     second_random = random.randrange(1, 10)
     random_date = datetime.date(random_year,random_month,random_day)
     print('TODAY IS ', random_date)
-    print('Random number between 0 and 1 is ', first_random)
+    print('Random number between 0 and 1 is {:3.2f} '.format(first_random))
     print('Random number between 1 and 10 is %d' % second_random)
     print('*' * 44)
     n += 1
