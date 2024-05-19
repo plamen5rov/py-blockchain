9eed137f624471a3cf463f8992d6eb7638441456
Plamen Petrov <biznetmen@gmail.com>
2024-05-19 10:32:23 +0300

    Splitting Up our Code

--- functions.py
+++
@@ -1,9 +0,0 @@
-def unlimited_arguments(*args):
-    print(args)
-    for arg in args:
-        print(arg)
-        
-        
-unlimited_arguments(1,2,3,4,5)
-unlimited_arguments(*[1,2,3,4,5])
-    
