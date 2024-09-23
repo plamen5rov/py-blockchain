a26fc4fe95e52fa5fac3777f2701716fe30ff61b
Plamen Petrov <biznetmen@gmail.com>
2024-09-23 18:50:20 +0300

    Returning Funds

--- node.py
+++ node.py
@@ -13,13 +13,14 @@ CORS(app)
 @app.route('/wallet', methods=['POST'])
 def create_keys():
     wallet.create_keys()
-    if wallet.save_keys():
-        response = {
-            'public_key': wallet.public_key,
-            'private_key': wallet.private_key
-        }
+    if wallet.save_keys(): 
         global blockchain
         blockchain = Blockchain(wallet.public_key)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
         return jsonify(response), 201
     else:
         response = {
@@ -31,16 +32,34 @@ def create_keys():
 @app.route('/wallet', methods=['GET'])
 def load_keys():
     if wallet.load_keys():
-        response = {
-            'public_key': wallet.public_key,
-            'private_key': wallet.private_key
-        }
         global blockchain
         blockchain = Blockchain(wallet.public_key)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
         return jsonify(response), 201
     else:
         response = {
             'message': 'Loading the keys failed.'
+        }
+        return jsonify(response), 500
+
+
+@app.route('/balance', methods=['GET'])
+def get_balance():
+    balance = blockchain.get_balance()
+    if balance != None:
+        response = {
+            'message': 'Fetched balance successfully.',
+            'funds': balance
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'messsage': 'Loading balance failed.',
+            'wallet_set_up': wallet.public_key != None
         }
         return jsonify(response), 500
 
@@ -59,7 +78,8 @@ def mine():
             tx.__dict__ for tx in dict_block['transactions']]
         response = {
             'message': 'Block added successfully.',
-            'block': dict_block
+            'block': dict_block,
+            'funds': blockchain.get_balance()
         }
         return jsonify(response), 201
     else:
